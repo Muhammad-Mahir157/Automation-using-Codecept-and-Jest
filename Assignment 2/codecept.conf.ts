220548ec25d666b3e5525c825ac61e48a7402745
Hammad <muhammadhammad1487@gmail.com>
2022-11-02 22:22:41 +0500

    Codecept Sample code

---
+++ Assignment 2/codecept.conf.ts
@@ -0,0 +1,22 @@
+import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+export const config: CodeceptJS.MainConfig = {
+  tests: './*_test.ts',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost',
+      show: true,
+      windowSize: '1200x900',
+      waitForNavigation: "networkidle0"
+    }
+  },
+  include: {},
+  name: 'Assignment 2'
+}
